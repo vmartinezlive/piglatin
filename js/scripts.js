3c3e26c419e02611cda8e20a551c6c70b6f03c33
Kristin Brewer-Lowe and Victoria Martinez <kristin@>
2018-10-24 14:57:18 -0700

    add function to tranform words that starts with constanant

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 //business logic
-var vowels = ['a', 'e', 'i', 'o', 'u'];
-var consonants = ['b', 'c' , 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y'];
+var vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
+var consonants = ['b', 'B', 'c', 'C', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z '];
+
 
 function translateToPigLatin(word){
-  if (isVowel(word)) {
-    return word+"way";
-  } else if (isConsonant(word)){
-    var letter = word.slice(0, 1);
-    var remainder = word.slice(1);
-    return remainder + letter + "ay";
+  if (startsWithVowel(word)) {
+    return word + "way";
+  } else if (startsWithConsonant(word)){
+    return transformConsonantWord(word);
   }
 }
+//make sure to return the function within the function
 
-function isVowel(word) {
+function startsWithVowel(word) {
   var letter = word.slice(0, 1);
   for (var i = 0; i < vowels.length; i++) {
     if (vowels.includes(letter)) {
@@ -23,9 +23,8 @@ function isVowel(word) {
   }
 }
 
-function isConsonant (word) {
+function startsWithConsonant (word) {
   var letter = word.slice(0, 1);
-  var remainder = word.slice(1);
   for (var i = 0; i < consonants.length; i++) {
     if (consonants.includes(letter)) {
       return true;
@@ -35,14 +34,23 @@ function isConsonant (word) {
   }
 }
 
+function transformConsonantWord (word) {
+  for (var i= 0; i < word.length; i++) {
+    var character = word.charAt(i);
+    if (startsWithVowel(character)) {
+      return word.slice(i) + word.slice(0, i) + "ay";
+    }
+  }
+}
+
 //user interface logic
 $(document).ready(function() {
  $('#pig-latin-form').submit(function(event) {
    event.preventDefault();
 
-   var phraseToTranslate = $('input#pig-latin').val();
+   var phraseToTranslate = ($('input#pig-latin').val()).toLowerCase();
    var result = translateToPigLatin(phraseToTranslate);
    $('#results').text(result);
-
+console.log(phraseToTranslate);
  });
 });
